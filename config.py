f0aea22d61569dd517b8dc0dfc070ecd97d9a896
Akshat Dutt Kaushik <144731308+majorwinger1316@users.noreply.github.com>
2025-09-21 12:02:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,5 @@ GITLAB_TOKEN = 'your_gitlab_token_here'
 BITBUCKET_KEY = 'your_bitbucket_key_here'
 
 BITBUCKET_SECRET = 'your_bitbucket_secret_here'
+
+WEBHOOK_SECRET = 'my_github_webhook_secret_123456!@#'
