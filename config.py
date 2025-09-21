132b24bf967cfc59d45ed1895f9c3c9e0a3dfc42
Akshat Dutt Kaushik <144731308+majorwinger1316@users.noreply.github.com>
2025-09-21 11:56:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
-import os
+GITHUB_TOKEN = "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"
 
-GROQ_API_KEY = os.environ.get('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', '')
-WEBHOOK_SECRET = os.environ.get('my_github_webhook_secret_123456!@#', '')
-GITHUB_TOKEN = os.environ.get('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', '')
-GITLAB_TOKEN = os.environ.get('GITLAB_TOKEN', '')
-BITBUCKET_KEY = os.environ.get('BITBUCKET_KEY', '')
-BITBUCKET_SECRET = os.environ.get('BITBUCKET_SECRET', '')
+GROQ_API_KEY = "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"
+
+GITLAB_TOKEN = 'your_gitlab_token_here'
+
+BITBUCKET_KEY = 'your_bitbucket_key_here'
+
+BITBUCKET_SECRET = 'your_bitbucket_secret_here'
