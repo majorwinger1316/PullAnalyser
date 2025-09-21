66d7c0fc02bac49d78aa9d923db2dc9c18d524b1
majorwinger1316 <ak4425@srmist.edu.in>
2025-09-21 10:55:59 +0530

    Initial PullAnalyser

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+GROQ_API_KEY = os.environ.get('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', '')
+WEBHOOK_SECRET = os.environ.get('my_github_webhook_secret_123456!@#', '')
+GITHUB_TOKEN = os.environ.get('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', '')
+GITLAB_TOKEN = os.environ.get('GITLAB_TOKEN', '')
+BITBUCKET_KEY = os.environ.get('BITBUCKET_KEY', '')
+BITBUCKET_SECRET = os.environ.get('BITBUCKET_SECRET', '')
